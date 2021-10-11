c2451078b12757f7b0f45305b8e9b1323b1a7db1
Cameron <cameronnagel8000@gmail.com>
2021-10-11 21:32:38 +0200

    Added "FileHandler" Class

---
+++ PRG282_Project/Data Access Layer/FileHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG282_Project.Data_Access_Layer
+{
+    internal class FileHandler
+    {
+    }
+}
